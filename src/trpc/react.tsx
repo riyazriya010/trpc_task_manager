5cc729918574b8f31741080f61b900e54796dbdf
riyazriya010 <riyur017@gmail.com.com>
2025-05-24 11:19:43 +0530

    change developement to production

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -46,7 +46,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
       links: [
         loggerLink({
           enabled: (op) =>
-            process.env.NODE_ENV === "development" ||
+            process.env.NODE_ENV === "production" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
         httpBatchStreamLink({
