def84cfbee2596b8bb2b25cd4915f242c59bdcbc
riyazriya010 <riyur017@gmail.com.com>
2025-05-24 13:05:01 +0530

    code cleanup

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,21 +1,3 @@
-// import { drizzle } from "drizzle-orm/postgres-js";
-// import postgres from "postgres";
-// import * as schema from "./schema";
-
-// // const connectionString = process.env.DATABASE_URL!;
-// const connectionString = "postgresql://postgres:riyaz@merN004@db.qgndfzrmesloopqzuodq.supabase.co:5432/postgres"
-// const client = postgres(connectionString, {max: 1}); // Supabase recommends keeping it small 
-// export const db = drizzle(client, { schema });
-
-
-// import { drizzle } from "drizzle-orm/postgres-js";
-// import postgres from "postgres";
-// import * as schema from "./schema";
-
-// const connectionString = process.env.DATABASE_URL!;
-// const client = postgres(connectionString, { max: 1 }); // Supabase recommends small pool
-// export const db = drizzle(client, { schema });
-
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
