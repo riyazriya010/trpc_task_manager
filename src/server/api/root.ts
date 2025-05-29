81b55eb39ce3d09926207652246767de75da2776
riyazriya010 <riyur017@gmail.com.com>
2025-05-29 13:17:14 +0530

    db url set for ipv4

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,15 +1,30 @@
 
 // This File combines all of our trpc routers to single appRouter, Which will be exposed to front-end
 
+// import { sayHello } from "./routers/hello";
+// import { taskRouter } from "./routers/task";
+// import { createTRPCRouter } from "./trpc";
+
+// export const appRouter = createTRPCRouter({
+//   hello: sayHello,
+//   task: taskRouter
+// })
+
+
+// // Export type definition of api
+// export type AppRouter = typeof appRouter;
+
+
 import { sayHello } from "./routers/hello";
 import { taskRouter } from "./routers/task";
-import { createTRPCRouter } from "./trpc";
+import { createTRPCRouter, createCallerFactory } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   hello: sayHello,
-  task: taskRouter
-})
+  task: taskRouter,
+});
 
-
-// Export type definition of api
 export type AppRouter = typeof appRouter;
+
+// Expose a `createCaller` for RSC
+export const createCaller = createCallerFactory(appRouter);
